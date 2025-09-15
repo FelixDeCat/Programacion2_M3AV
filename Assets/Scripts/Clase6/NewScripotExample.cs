a4407bfbf20f1592712ce337d4602877501f851c
Guillermo Felix Romero <guillermofelixromero@gmail.com>
2025-09-15 10:30:03 -0300

    newScript + CharMove

--- Assets/Scripts/Clase6/NewScripotExample.cs
+++ Assets/Scripts/Clase6/NewScripotExample.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class thirdPersonChar : MonoBehaviour
+public class NewScripotExample : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class thirdPersonChar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 intput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
     }
 }
